767134ecc0d37bfa33a8898b62fc50473b6fbb4f
Tariteur <kevin.muel35@gmail.com>
2023-12-01 18:19:05 +0100

    2

---
+++ game/js/controls.js
@@ -0,0 +1,67 @@
+import { camera, playerDirection, playerOnFloor, keyStates, playerVelocity } from './main';
+
+function getForwardVector() {
+
+    camera.getWorldDirection(playerDirection);
+    playerDirection.y = 0;
+    playerDirection.normalize();
+
+    return playerDirection;
+
+}
+function getSideVector() {
+
+    camera.getWorldDirection(playerDirection);
+    playerDirection.y = 0;
+    playerDirection.normalize();
+    playerDirection.cross(camera.up);
+
+    return playerDirection;
+
+}
+export function controls(deltaTime) {
+
+    // gives a bit of air control
+    let speedDelta = deltaTime * (playerOnFloor ? 25 : 8) / 2;
+
+    if (keyStates['ShiftLeft']) {
+
+        speedDelta *= 2;
+
+    }
+
+    if (keyStates['KeyW']) {
+
+        playerVelocity.add(getForwardVector().multiplyScalar(speedDelta));
+
+    }
+
+    if (keyStates['KeyS']) {
+
+        playerVelocity.add(getForwardVector().multiplyScalar(-speedDelta));
+
+    }
+
+    if (keyStates['KeyA']) {
+
+        playerVelocity.add(getSideVector().multiplyScalar(-speedDelta));
+
+    }
+
+    if (keyStates['KeyD']) {
+
+        playerVelocity.add(getSideVector().multiplyScalar(speedDelta));
+
+    }
+
+    if (playerOnFloor) {
+
+        if (keyStates['Space']) {
+
+            playerVelocity.y = 7.5;
+
+        }
+
+    }
+
+}
